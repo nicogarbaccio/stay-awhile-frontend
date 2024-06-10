5b79619f64dab89ef85a8a6641e5785a3ef88f45
Nico Garbaccio <garbaccio20@gmail.com>
2024-06-10 12:30:19 -0400

    chat detail page, custom button component

---
+++ app/components/forms/CustomButton.tsx
@@ -0,0 +1,22 @@
+interface CustomButtonProps {
+  label: string;
+  onClick: () => void;
+  className?: string;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  label,
+  onClick,
+  className,
+}) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`w-full py-4 bg-primary hover:bg-dark text-white text-center rounded-xl transition cursor-pointer ${className}`}
+    >
+      {label}
+    </div>
+  );
+};
+
+export default CustomButton;
