0458d98fcf22c66045a4a98380dc8f3d17086b74
Nico Garbaccio <garbaccio20@gmail.com>
2024-06-11 18:38:12 -0400

    create reusable modal window component

--- app/components/navbar/UserNav.tsx
+++ app/components/navbar/UserNav.tsx
@@ -4,11 +4,11 @@ import { useState } from "react";
 import MenuLink from "./MenuLink";
 
 const UserNav = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="p-2 relative inline-block border rounded-full">
-      <button className="flex items-center">
+      <button onClick={() => setIsOpen(!isOpen)} className="flex items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
